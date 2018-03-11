029626189077af875e416cb085b1c87254b3254b
Arpit Sharma <s.arpit@iitg.ernet.in>
2018-03-11 13:57:03 +0530

    Intext search, sort, delete in working condition

---
+++ helper.py
@@ -0,0 +1,53 @@
+import sqlite3, tweepy
+from tweepy import OAuthHandler, Stream
+from tweepy.streaming import StreamListener
+
+
+conn = sqlite3.connect("twitter.db")
+c = conn.cursor()
+
+ckey="Consumer_Key"
+csecret="Consumer_Secret"
+atoken= "Access_Token"
+asecret="Access_Secret_Token"
+
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+api = tweepy.API(auth)
+
+'''
+c.execute("""CREATE TABLE twitter (
+			tweet_id int,
+			Tweet text,
+			User text,
+			ScreenName text,
+			HashTags text,
+			Topics text,
+			datestamp text,
+			Favorite int,
+			Retweet int,
+			Language text
+			)""")
+'''
+
+
+def get_trends():
+	#WOEID for india is 23424848
+	trend = api.trends_place(id=23424848)
+	return trend
+
+def get_tweets(query):
+	data = api.search(query, result_type='recent')
+	for status in data:
+		topic = query
+		hashs = ''
+		for hash in status.entities["hashtags"]:
+			hashs = hashs + ' '+hash["text"] +','
+			topic = topic + ', '+hash["text"]
+		for mention in status.entities["user_mentions"]:
+			topic = topic + ', '+mention["name"]
+
+		c.execute("INSERT INTO twitter VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (status.id, status.text, status.author.name, status.author.screen_name, hashs, topic, str(status.created_at), status.favorite_count, status.retweet_count, status.lang))
+		conn.commit()
+	return True
---
+++ twitter.py
@@ -0,0 +1,64 @@
+import sqlite3, time, helper
+from flask import Flask, redirect, render_template, request, url_for, flash
+import csv
+
+
+app = Flask(__name__)
+
+conn = sqlite3.connect("twitter.db")
+c = conn.cursor()
+
+# Display Trending topics on twitter
+@app.route("/", methods=["GET", "POST"])
+def home():
+	#get trending topics in India
+	trends = helper.get_trends()
+
+	return render_template("index.html", trends=trends)
+
+
+@app.route("/query", methods=["GET", "POST"])
+def query():
+
+	trends = helper.get_trends()
+	q = request.args.get('query').lower()
+	order = request.args.get('orderby')
+	data = []
+	if order == 'Retweet':
+		c.execute("SELECT * FROM twitter ORDER BY Retweet DESC")
+	elif order == 'Favorite_count':
+		c.execute("SELECT * FROM twitter ORDER BY Favorite_count DESC")
+	else:
+		c.execute("SELECT * FROM twitter ORDER BY tweet_id DESC")
+
+	tweets = c.fetchall()
+
+	msg = None
+	if not tweets:
+		return render_template("query.html", msg='No Tweets related to %s. Sorry!'%q)
+	else:
+		for tweet in tweets:
+			if q in tweet[1].lower() or q in tweet[2].lower() or q in tweet[3].lower() or q in tweet[4].lower() or q in tweet[5].lower():
+				data.append(tweet)
+
+		return render_template("query.html", query=q, trends=trends, tweets=data)
+
+# Save searched tweets into Database
+@app.route("/search")
+def search():
+	q = request.args.get('query')
+	helper.get_tweets(q)
+	return redirect(url_for("home"))
+
+
+@app.route("/delete", methods=["GET", "POST"])
+def delete():
+	delete_term = request.args.get('delete')
+	if delete_term:
+		c.execute("DELETE FROM twitter WHERE Topics='%s'"%delete_term)
+		conn.commit()
+
+	else:
+		c.execute("DELETE FROM twitter")
+		conn.commit()
+	return redirect(url_for("home"))
